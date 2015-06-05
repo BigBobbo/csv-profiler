46390f9236327a1073ce4ca9a1bd32e0a4a26917
BigBobbo <mrrocallaghan@gmail.com>
2015-06-05 23:56:31 +0000

    Added bootstrap and static files

--- app2/__init__.py
+++ app2/__init__.py
@@ -30,16 +30,7 @@ def upload_file():
             file.save(os.path.join(ip_dir, filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <h2>''' + str(cwd) + '''</h2>
-    <form action="" method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+    return render_template("upload.html")
 
 
 
