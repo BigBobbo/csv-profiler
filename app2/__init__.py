961169c9c3d5fb89357aa07e0364006dca76656f
BigBobbo <mrrocallaghan@gmail.com>
2015-06-05 19:56:33 +0000

    Upgraded security to only allow users access there own files

--- app2/__init__.py
+++ app2/__init__.py
@@ -1,10 +1,10 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, request
 from werkzeug import secure_filename
 import pandas as pd
 import numpy as np
 from .profiler import profileGen
-
+import shutil
 
 UPLOAD_FOLDER = '/home/csvprofiler/csv-profiler/app2/uploads'
 cwd = os.getcwd()
@@ -12,6 +12,7 @@ ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 
 def allowed_file(filename):
@@ -23,8 +24,10 @@ def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
+            ip_dir = os.path.join(app.config['UPLOAD_FOLDER'], request.headers['X-Real-IP'].replace(".", ""))
+            os.mkdir(ip_dir)
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(ip_dir, filename))
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     return '''
@@ -42,13 +45,15 @@ def upload_file():
 
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
-    # x = pd.DataFrame.from_csv(app.config['UPLOAD_FOLDER'] + "/" + filename)
+    ip_dir = os.path.join(app.config['UPLOAD_FOLDER'], request.headers['X-Real-IP'].replace(".", ""))
+    x = profileGen(ip_dir + "/" + filename)
+    shutil.rmtree(ip_dir)
+    # x = profileGen(app.config['UPLOAD_FOLDER'] + "/" + filename)
     # os.remove(app.config['UPLOAD_FOLDER'] + "/" + filename)
-    x = profileGen(app.config['UPLOAD_FOLDER'] + "/" + filename)
     return render_template("analysis.html", name=filename, data=x.to_html())
 
 
 @app.route('/analysis/<filename>')
 def analysis(filename):
     x = pd.DataFrame(np.random.randn(20, 5))
-    return render_template("analysis.html", name=filename, data=x.to_html())
+    return render_template("analysis.html", name=request.headers['X-Real-IP'], data=x.to_html())
